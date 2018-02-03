1992178532e05734ab059b720ebfeeaaf37409c6
TOMITA Masahiro <tommy@tmtm.org>
2018-02-03 20:24:04 +0900

    0.2

--- docs/plugin/rabbit/rabbit.js
+++ docs/plugin/rabbit/rabbit.js
@@ -2,27 +2,46 @@
     var start_time = null;
 
     var img_rabbit = document.createElement('img');
-    img_rabbit.setAttribute('src', 'images/rabbit/rabbit.png');
-    img_rabbit.setAttribute('style', 'position:fixed; bottom:0px; left: 0px; height:15%; display:none');
+    img_rabbit.setAttribute('id', 'rabbit-rabbit');
+    img_rabbit.setAttribute('style', 'position:fixed; bottom:0px; left: 0px; visibility:hidden');
+
     var img_turtle = document.createElement('img');
-    img_turtle.setAttribute('src', 'images/rabbit/turtle.png');
-    img_turtle.setAttribute('style', 'position:fixed; bottom:0px; left: 0px; height:15%; display:none');
+    img_turtle.setAttribute('id', 'rabbit-turtle');
+    img_turtle.setAttribute('style', 'position:fixed; bottom:0px; left: 0px; visibility:hidden');
+
     document.body.appendChild(img_turtle);
     document.body.appendChild(img_rabbit);
+
+    // CSS background-image の URL を src に設定して background-image を削除
+    var set_image_src = function(img) {
+        var bg_image_url = window.getComputedStyle(img).backgroundImage.match(/^url\(\"([^\"]*)\"\)/);
+        if (bg_image_url) {
+            img.src = bg_image_url[1];
+            img.style.backgroundImage = 'none';
+        }
+    }
+
+    set_image_src(img_rabbit);
+    set_image_src(img_turtle);
 
     setInterval(function(){
         if (start_time) {
             var alloted_time = Reveal.getConfig().alloted_time;
             if (alloted_time) {
-                img_turtle.style.display = '';
-                img_turtle.style.left = (window.innerWidth - img_turtle.width) * ((Date.now()-start_time) / 1000 / alloted_time) + "px";
+                var left = (window.innerWidth - img_turtle.width) * ((Date.now()-start_time) / 1000 / alloted_time) * 100 / window.innerWidth;
+                if (left > 100) {
+                    img_turtle.style.visibility = 'hidden';
+                } else {
+                    img_turtle.style.visibility = 'visible';
+                    img_turtle.style.left = left + '%';
+                }
             } else {
-                img_turtle.style.display = 'none';
+                img_turtle.style.visibility = 'visible';
             }
         }
     }, 500);
 
-    var rabbit = function(current_page) {
+    var display_rabbit = function(current_page) {
         if (!start_time) {
             var re = /rabbit_start_time=(\d+)/;
             if (current_page > 0 && document.cookie.match(re)) {
@@ -33,8 +52,9 @@
             }
         }
         var total_page = Reveal.getTotalSlides();
-        img_rabbit.style.display = '';
-        img_rabbit.style.left = (window.innerWidth - img_rabbit.width) * (current_page / (total_page - 1)) + "px";
+        var left = (window.innerWidth - img_rabbit.width) * (current_page / (total_page - 1)) * 100 / window.innerWidth;
+        img_rabbit.style.visibility = 'visible';
+        img_rabbit.style.left = left + '%';
     }
     var current_page = Reveal.getIndices().h
     if (current_page == 0) {
@@ -42,8 +62,7 @@
             document.cookie = 'rabbit_start_time=';
         }
     } else {
-        setTimeout(function(){rabbit(current_page)}, 0);
+        setTimeout(function(){display_rabbit(current_page)}, 0);
     }
-    Reveal.addEventListener('slidechanged', function(event){rabbit(event.indexh)});
+    Reveal.addEventListener('slidechanged', function(event){display_rabbit(event.indexh)});
 }());
-
--- reveal-ck-rabbit-plugin.gemspec
+++ reveal-ck-rabbit-plugin.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |spec|
   spec.name          = "reveal-ck-rabbit-plugin"
-  spec.version       = '0.1'
+  spec.version       = '0.2'
   spec.licenses      = ['MIT']
   spec.authors       = ["TOMITA Masahiro"]
   spec.email         = ["tommy@tmtm.org"]
@@ -9,12 +9,11 @@ Gem::Specification.new do |spec|
   spec.description   = "reveal-ck rabbit plugin"
   spec.homepage      = "http://github.com/tmtm/reveal-ck-rabbit-plugin"
 
-  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
-    f.match(%r{^(test|spec|features)/})
+  spec.files         = `git ls-files -z`.split("\x0").select do |f|
+    f.match(%r{^(LICENSE|README.md|css|exe|images|plugin)/})
   end
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
